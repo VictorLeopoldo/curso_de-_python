886ec913cdaf7931478eacbbd766bc7258c40660
VictorLeopoldo <victorleovazavi@gmail.com>
2024-03-05 22:16:08 -0300

    Adicionado o Numero Reais

--- numeros_inteiros.py
+++ numeros_inteiros.py
@@ -3,7 +3,7 @@ Preciso criar um programa que receba um número inteiro e o imprima"""
 
 
 #Atribuindo um número a um variável
-#Assigning a number to a variable.
+#Assigning a number to a variable
 num = 23
 
 #Imprima a variável na tela
---
+++ numeros_reais.py
@@ -0,0 +1,9 @@
+"""
+Este programa terá que receber um número real e imprimir na tela
+"""
+#Atribuindo um número a um variável
+#Assigning a number to a variable
+num = 3.1415
+#Imprima a variável na tela
+#Print the variable on the screen
+print(num)
