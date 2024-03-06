a14bda2740da0136ed924bb5ed5a08343989f6d3
VictorLeopoldo <victorleovazavi@gmail.com>
2024-03-05 22:00:11 -0300

    Programa Números Inteiros

---
+++ numeros_inteiros.py
@@ -0,0 +1,11 @@
+"""Este programa faz parte do exerecício de fixação e aprendizado em python
+Preciso criar um programa que receba um número inteiro e o imprima"""
+
+
+#Atribuindo um número a um variável
+#Assigning a number to a variable.
+num = 23
+
+#Imprima a variável na tela
+#Print the variable on the screen
+print(num)
