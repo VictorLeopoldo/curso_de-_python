8c0d6e87211421d30b028dfdab98691b52ada9bb
VictorLeopoldo <victorleovazavi@gmail.com>
2024-03-06 23:28:13 -0300

    Divisão de um numero

---
+++ quinta_parte_de_um_numero.py
@@ -0,0 +1,14 @@
+"""
+Programa para ler um número real e imprima a quinta parte deste número
+
+Program to read a real number and print one-fifth of this number
+"""
+#Solicita ao usuário digitar um número e adiciona o número na variavel
+#Solicits the user to enter a number and adds the number to the variable
+num = float(input("Digite o número "))
+#Realiza a operação de divisão do número
+#Performs the division operation of the number
+qui_par = num /5
+#Imprime o resultado final para o usuário
+#Prints the final results for the user
+print("A quinta parte do número que você digitou é: ", qui_par)
