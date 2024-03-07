dc5b4dc3d758966c6f10ebd60d125580351e3cc8
VictorLeopoldo <victorleovazavi@gmail.com>
2024-03-06 23:13:08 -0300

    Eleva um numero ao quadrado

---
+++ elevado_ao_quadrado.py
@@ -0,0 +1,19 @@
+"""
+Faça um programa que leia um número e imprima o quadrado
+desse número
+
+Here's a program that reads a number and prints the square of that
+number
+"""
+
+#Adiciona um valor a variável
+#Adds a value to the variable
+num = 23
+
+#Eleva a variável ao quadrado
+#Squares the variable
+qua = num ** 2
+
+#Imprime o resultado
+#Prints the result
+print(qua)
