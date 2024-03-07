e36130a1966de3f6df0c4f2e280ba0fc9957d456
VictorLeopoldo <victorleovazavi@gmail.com>
2024-03-06 22:44:26 -0300

    Adicionando Soma de Três Números

---
+++ soma_três_numeros.py
@@ -0,0 +1,26 @@
+"""
+Faça um programa de peça ao usuário digitar 3 números interios
+e imprima a soma deles.
+
+This program asks the user to input 3 integers and prints their sum.
+
+"""
+#Solicita ao usuário digitar um número inteiro e atribui na variavel
+#Ask the user to enter an integer and assigns it to a variable
+n1 = int(input("Digite o primeiro número interiro: "))
+
+#Solicita ao usuário digitar um número inteiro e atribui na variavel
+#Ask the user to enter an integer and assigns it to a variable
+n2 = int(input("Digite o segundo número interiro: "))
+
+#Solicita ao usuário digitar um número inteiro e atribui na variavel
+#Ask the user to enter an integer and assigns it to a variable
+n3 = int(input("Digite o terceiro número interiro:"))
+
+#Realiza a soma dos números informados pelo usuário
+#Performs the sum of the numbers provided by the user
+soma = n1 + n2 + n3
+
+#Imprime o resultado final
+#Prints the final result
+print("A soma dos números que digitou é: ", soma)
