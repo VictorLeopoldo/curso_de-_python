47caabc2cb62501df0579676d442a3ce7f06f886
VictorLeopoldo <victorleovazavi@gmail.com>
2024-03-07 21:44:11 -0300

    Converso de Kelvin

---
+++ conversor_kelvin.py
@@ -0,0 +1,11 @@
+"""
+Este programa é um conversor de graus Kelvin para Celsius
+
+This program is converso de degrees Kelvin for Celsius
+"""
+print("Este é um conversor de Temperatura de Kelvin para Celsius")
+kel = float(input("Informe o valor da temperatura em Kelvin para a conversão: "))
+
+cel = kel - 273.15
+
+print("A converão de: ", kel, "Kelvin \npara: ", cel, "Celsius")
